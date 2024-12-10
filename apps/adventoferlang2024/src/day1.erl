1f017cc4863b91ceae1ca6fc7a64f67897309bb7
zerowaitstate <danielwaites@proton.me>
2024-12-10 16:57:45 -0600

    finished day1 part 2

--- apps/adventoferlang2024/src/day1.erl
+++ apps/adventoferlang2024/src/day1.erl
@@ -4,7 +4,7 @@
 
 -module(day1).
 
--export([part1/0]).
+-export([part1/0, part2/0]).
 
 load_file(Fn) ->
     {ok, Device} = file:open(Fn, [read]),
@@ -18,13 +18,38 @@ load_file_loop(IoDevice) ->
         {ok, [First|[Second]]} -> [{First,Second}|load_file_loop(IoDevice)]
     end.
 
-process_input(TwoTupleList) ->
+puzzle_input() ->
+    Fn = "day1.txt",
+    AbsFn = filename:absname_join(code:priv_dir(adventoferlang2024), Fn),
+    load_file(AbsFn).
+
+%%%-----------------------------------
+%% part 1 stuff
+%%%-----------------------------------
+
+sort_diff_sum(TwoTupleList) ->
     {L1, L2} = lists:unzip(TwoTupleList),
     L1_Sorted = lists:sort(L1),
     L2_Sorted = lists:sort(L2),
     lists:sum(lists:zipwith(fun(X,Y) -> abs(X-Y) end, L1_Sorted, L2_Sorted)).
 
 part1() ->
-    Fn = "day1.txt",
-    Data = load_file(filename:absname_join(code:priv_dir(adventoferlang2024), Fn)),
-    process_input(Data).
+    sort_diff_sum(puzzle_input()).
+
+%%%-----------------------------------
+%% part 2 stuff
+%%%-----------------------------------
+
+get_freq_product(FreqTable, E) ->
+    Freq = case ets:lookup(FreqTable, E) of
+        [] -> 0;
+        [{_Key, Val}] -> Val
+    end,
+    E * Freq.
+
+part2() ->
+    FreqT = ets:new(freq, [set]),
+    Input = puzzle_input(),
+    {L1, L2} = lists:unzip(Input),
+    lists:foreach(fun(E) -> ets:update_counter(FreqT, E, 1, {0, 0}) end, L2),
+    lists:sum(lists:map(fun(E) -> get_freq_product(FreqT, E) end, L1)).
