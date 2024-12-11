5ba55dc94ef92b2e2c5724f1dad913f607467e99
zerowaitstate <danielwaites@proton.me>
2024-12-11 08:45:12 -0600

    wip

--- apps/adventoferlang2024/src/day2.erl
+++ apps/adventoferlang2024/src/day2.erl
@@ -4,7 +4,7 @@
 
 -module(day2).
 
--export([part1/0, part2/0]).
+-export([part1/0, part2/0, summarize/2]).
 
 load_file(Fn) ->
     {ok, Device} = file:open(Fn, [read]),
@@ -29,25 +29,26 @@ puzzle_input() ->
 %% part 1 stuff
 %%%------------------------------
 
-summarize([FirstTerm|[]]) -> {ok, FirstTerm, none};
-summarize([FirstTerm|Rest]) ->
-    case summarize(Rest) of
-        {ok, SecondTerm, none} when (FirstTerm < SecondTerm) and (abs(FirstTerm - SecondTerm) =< 3) ->
-            {ok, FirstTerm, inc};
-        {ok, SecondTerm, none} when (FirstTerm > SecondTerm) and (abs(FirstTerm - SecondTerm) =< 3) ->
-            {ok, FirstTerm, dec};
-        {ok, SecondTerm, inc} when (FirstTerm < SecondTerm) and (abs(FirstTerm - SecondTerm) =< 3) ->
-            {ok, FirstTerm, inc};
-        {ok, SecondTerm, dec} when (FirstTerm > SecondTerm) and (abs(FirstTerm - SecondTerm) =< 3) ->
-            {ok, FirstTerm, dec};
-        {failed, SecondTerm, Dir} ->
-            {failed, SecondTerm, Dir};
-        {ok, SecondTerm, Dir} ->
-            {failed, SecondTerm, Dir}
+summarize([FirstTerm|[]], Misses) -> {ok, FirstTerm, none, Misses, 1};
+summarize([FirstTerm|Rest], Misses) ->
+    Result = summarize(Rest, Misses),
+    case Result of
+        {ok, SecondTerm, none, Rem, Levels} when (FirstTerm < SecondTerm) and (abs(FirstTerm - SecondTerm) =< 3) ->
+            {ok, FirstTerm, inc, Rem, Levels + 1};
+        {ok, SecondTerm, none, Rem, Levels} when (FirstTerm > SecondTerm) and (abs(FirstTerm - SecondTerm) =< 3) ->
+            {ok, FirstTerm, dec, Rem, Levels + 1};
+        {ok, SecondTerm, inc, Rem, Levels} when (FirstTerm < SecondTerm) and (abs(FirstTerm - SecondTerm) =< 3) ->
+            {ok, FirstTerm, inc, Rem, Levels + 1};
+        {ok, SecondTerm, dec, Rem, Levels} when (FirstTerm > SecondTerm) and (abs(FirstTerm - SecondTerm) =< 3) ->
+            {ok, FirstTerm, dec, Rem, Levels + 1};
+        {ok, SecondTerm, Dir, Rem, Levels} when Rem > 0 ->
+            {ok, SecondTerm, Dir, Rem - 1, Levels + 1};
+        _Else ->
+            {failed}
     end.
 
 part1() ->
-    Results = lists:filter(fun(X) -> {Res, _, _} = summarize(X), Res == ok end, puzzle_input()),
+    Results = lists:filter(fun(X) -> element(1,summarize(X, 0)) == ok end, puzzle_input()),
     length(Results).
 
 %%%------------------------------
@@ -55,4 +56,5 @@ part1() ->
 %%%------------------------------
 
 part2() ->
-    ok.
+    Results = lists:filter(fun(X) -> element(1,summarize(X, 1)) == ok end, puzzle_input()),
+    length(Results).
